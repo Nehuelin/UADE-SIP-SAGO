71d6bc1dcd008a77bf2477530ba04935b7b991ef
Nehuelin <nehuel2005@gmail.com>
2025-05-20 18:03:25 -0300

    + enrutamiento a nuevas paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
-
+import LandingPage from './components/LandingPage/LandingPage.jsx';
+import LoginForm from './components/LoginForm/LoginForm.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
-
 function App() {
-
-  return (
-    <>
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <SignUpForm />
-        </div>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/register" element={<SignUpForm />} />
+                <Route path="/login" element={<LoginForm />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default App;
