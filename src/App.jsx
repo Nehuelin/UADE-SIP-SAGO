5bdff46f9bd511a73ff6202cfbc584ae597681da
Nehuelin <nehuel2005@gmail.com>
2025-05-20 19:43:32 -0300

    + enrutamiento a menu principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
 import LandingPage from './components/LandingPage/LandingPage.jsx';
 import LoginForm from './components/LoginForm/LoginForm.jsx';
+import TestPlayground from './components/DEV_ONLY/TestPlayground.jsx';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import MainMenu from "./components/MainMenu/MainMenu.jsx";
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/*<Route path="/" element={<TestPlayground />} />*/}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/register" element={<SignUpForm />} />
                 <Route path="/login" element={<LoginForm />} />
+                <Route path="/main-menu" element={<MainMenu/>} />
             </Routes>
         </BrowserRouter>
     )
