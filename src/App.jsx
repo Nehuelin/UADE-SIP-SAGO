e2b287fbeb77925564d1b6afbddf4ac4354a45cf
Nehuelin <nehuel2005@gmail.com>
2025-05-23 18:32:09 -0300

    + renombramiento de rutas y se agrego la ruta a la Agenda

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css'
+
 import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
 import LandingPage from './components/LandingPage/LandingPage.jsx';
 import LoginForm from './components/LoginForm/LoginForm.jsx';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
 import MainMenu from "./components/MainMenu/MainMenu.jsx";
-
+import TestPlayground from "./components/DEV_ONLY/TestPlayground.jsx";
+import AgendaMenu from "./components/Agenda/AgendaMenu/AgendaMenu.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/*<Route path="/" element={<TestPlayground />}/>*/}
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/register" element={<SignUpForm />} />
+                <Route path="/registracion" element={<SignUpForm />} />
                 <Route path="/login" element={<LoginForm />} />
-                <Route path="/main-menu" element={<MainMenu/>} />
+                <Route path="/menu-principal" element={<MainMenu/>} />
+                <Route path="/agenda" element={<AgendaMenu/>} />
             </Routes>
         </BrowserRouter>
     )
