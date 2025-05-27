ea34bf8c5374850c08cf44cd5ae1394368b44f68
Alejandro L. <alejandrolopez1246m@gmail.com>
2025-05-27 10:48:21 -0300

    Creación de carpeta firebase. Conección con la base de datos

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqenbtHahd-OZUF_XfmZLRwWXgQQAlNoc",
+  authDomain: "sago-46988.firebaseapp.com",
+  projectId: "sago-46988",
+  storageBucket: "sago-46988.firebasestorage.app",
+  messagingSenderId: "876912557800",
+  appId: "1:876912557800:web:3dc324020f4d0c4035d35d",
+  measurementId: "G-ZHMK464DER"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
