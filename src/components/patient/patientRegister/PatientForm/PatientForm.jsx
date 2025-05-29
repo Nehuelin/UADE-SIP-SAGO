2cd3f4bbcef5e67923f5702ef51f65f451f64728
Nehuelin <nehuel2005@gmail.com>
2025-05-29 20:23:00 -0300

    + reasignacion de carpetas

--- src/components/patient/patientRegister/PatientForm/PatientForm.jsx
+++ src/components/patient/patientRegister/PatientForm/PatientForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { simulateAuth } from '../../../helpers/authSimulator.js';
-import Loading from '../../common/Loading/Loading.jsx';
+import { simulateAuth } from '../../../../helpers/authSimulator.js';
+import Loading from '../../../common/Loading/Loading.jsx';
 import SuccessScreen from "../SuccessScreen/SuccessScreen.jsx";
 import './PatientForm.css';
 
