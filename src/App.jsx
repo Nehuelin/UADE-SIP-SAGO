63e1d28252de5e0752efb6163575e4b93f5b6490
Nehuelin <nehuel2005@gmail.com>
2025-05-29 20:21:09 -0300

    + bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import SignUpForm from "./components/auth/SignUpForm/SignUpForm.jsx";
 import LandingPage from './components/landing/LandingPage/LandingPage.jsx';
 import LoginForm from './components/auth/LoginForm/LoginForm.jsx';
 import MainMenu from "./components/common/MainMenu/MainMenu.jsx";
-import TestPlayground from "./components/dev/TestPlayground.jsx";
 import AgendaMenu from "./components/agenda/AgendaMenu/AgendaMenu.jsx";
-import RecoverForm from "./components/auth/RecoverForm/RecoverForm.jsx";
-import PatientForm from "./components/patientRegister/PatientForm/PatientForm.jsx";
+import PatientForm from "./components/patient/patientRegister/PatientForm/PatientForm.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/*<Route path="/" element={<TestPlayground />}/>*/}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/registracion" element={<SignUpForm />} />
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/menu-principal" element={<MainMenu/>} />
                 <Route path="/agenda" element={<AgendaMenu/>} />
-                <Route path="/buscar-pacientes" element={<PatientForm/>} />
+                <Route path="/registrar-paciente" element={<PatientForm/>} />
             </Routes>
         </BrowserRouter>
     )
