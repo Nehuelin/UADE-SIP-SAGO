a7185bdf945a27904d8ed4603a18785610ebd8bd
Nehuelin <nehuel2005@gmail.com>
2025-05-31 18:01:57 -0300

    + componente para mensajes tipo 404

---
+++ src/components/common/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import './NotFound.css';
+
+export default function NotFound() {
+    return (
+        <div className="not-found">
+            <h2>404 - Página no encontrada</h2>
+            <p>Lo sentimos, la página que usted esta buscando no existe... todavia</p>
+        </div>
+    );
+}
